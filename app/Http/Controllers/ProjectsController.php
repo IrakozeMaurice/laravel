7808428abbdca465423e6ef2884c2314d91b10a3
Irakoze Maurice <iramaurimade@gmail.com>
2020-12-28 11:21:34 +0200

    Form handling and csrf protection

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -10,6 +10,22 @@ class ProjectsController extends Controller
     public function index(){
         $projects = Project::all();
         return view('projects.index' , ['projects' => $projects]);
-        // return $projects;
+    }
+
+    public function create(){
+        return view('projects.create');
+    }
+
+    public function store(){
+        //create a new project
+        $project = new Project;
+        $project -> title = request('title');
+        $project -> description = request('description');
+
+        //save the project in the database
+        $project -> save();
+
+        //redirect back to projects homepage
+        return \redirect('/projects');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,11 @@
 |
 */
 
-use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/' , 'PagesController@home');
 Route::get('/about' , 'PagesController@about');
 Route::get('/contact' , 'PagesController@contact');
 Route::get('/projects' , 'ProjectsController@index');
+Route::post('/projects' , 'ProjectsController@store');
+Route::get('/projects/create' , 'ProjectsController@create');
