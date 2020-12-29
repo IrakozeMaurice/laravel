ba41a7dc6958d3983b30d85de81ec388933f6f85
Irakoze Maurice <iramaurimade@gmail.com>
2020-12-29 12:13:52 +0200

    Updating tasks

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -4,14 +4,24 @@
 @section('content')
     <h1>{{ $project->title }}</h1>
     <p>{{ $project->description }}</p>
-    <br>
     <a href="/projects/{{ $project->id }}/edit">Edit</a>
+    <br><br>
+
     @if ($project->tasks->count())
         <div>
             @foreach ($project->tasks as $task)
-                <li>{{ $task->description }}</li>
-                <span>Completed: {{ $task->completed }}
+                <div>
+                    <form method="POST" action="/tasks/{{ $task->id }}">
+                        @method('PATCH')
+                        @csrf
+                        <label for="completed" class="{{ $task->completed ? 'isChecked' : '' }}">
+                            <input type="checkbox" name="completed" onchange="this.form.submit()"
+                                {{ $task->completed ? 'checked' : '' }}>
+                            {{ $task->description }}
+                    </form>
+                </div>
             @endforeach
         </div>
     @endif
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::resource('projects', 'ProjectsController');
 
+Route::patch('/tasks/{task}', 'ProjectTasksController@update');
