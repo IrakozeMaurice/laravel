c2ffff8746c312128e75e0c8d9c60525781412dc
Irakoze Maurice <iramaurimade@gmail.com>
2021-01-04 11:58:00 +0200

    middleware:Authorization

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -13,7 +13,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
+        'App\Project' => 'App\Policies\ProjectPolicy',
     ];
 
     /**
@@ -25,6 +25,10 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::before(function ($user) {
+
+            return $user->id == 2;  //this is admin id
+
+        });
     }
 }
